fbe3edca7b4562df807e810a72d2fa0c1c3f64cd
ricklegac <rick.legac@gmail.com>
2022-10-13 22:06:01 -0300

    solucionado error

--- utils/app.py
+++ utils/app.py
@@ -1,18 +1,17 @@
+# search.py
+# ---------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to Clemson University and the authors.
+# 
+# Authors: Pei Xu (peix@g.clemson.edu) 
+#
 
 import random, time
 import tkinter as tk
 from .utils import ordinal
-"""
-la clase Board() simula el tablero 
-las siguientes propiedades:
-    b.rows  numero de filas en la tabla
-    b.cols  numero de columnas en la tabla
-    b.PLAYER1   bandera que representa al jugador 1 
-    b.PLAYER2   bandera que representa al jugador 2
-    b.EMPTY_SLOT    bandera que representa un espacio vacio en el tablero
-    
 
-"""
 class Board(object):
 
     EMPTY_SLOT = 0
@@ -38,11 +37,11 @@ class Board(object):
     def placeable(self, col):
         return self._board[0][col] == self.EMPTY_SLOT
 
-    def place(self, jugador, col):
-        assert(jugador == self.PLAYER1 or jugador == self.PLAYER2)
+    def place(self, player, col):
+        assert(player == self.PLAYER1 or player == self.PLAYER2)
         for r in reversed(self._board):
             if r[col] == self.EMPTY_SLOT:
-                r[col] = jugador
+                r[col] = player
                 return True
         raise ValueError("Column {} is not placeable.".format(col))
 
@@ -108,16 +107,16 @@ class App(tk.Frame):
         super().__init__(master)
         self.alg_fn_map = alg_fn_map
 
-        self.master.title("Busqueda Adversario -- Conecta4")
+        self.master.title("Adversarial Search -- CPSC 4820/6820 Clemson University")
 
         self.master.geometry("640x480")
         self.master.resizable(False, False)
 
-        self.canvas = tk.Canvas(self.master, bg="black")
+        self.canvas = tk.Canvas(self.master, bg="white")
         self.canvas.grid(row=0, column=0, columnspan=4,
             sticky=tk.W+tk.E+tk.N+tk.S, padx=10, pady=28
         )
-        self.bt_new = tk.Button(self.master, text="Nuevo Juego", command=self.new_game)
+        self.bt_new = tk.Button(self.master, text="New", command=self.new_game)
         self.bt_new.grid(row=1, column=0,
             sticky=tk.W, padx=10, pady=(0, 10)
         )
@@ -140,7 +139,7 @@ class App(tk.Frame):
             return
 
         self.new_game_window = tk.Toplevel(self.master)
-        self.new_game_window.title("Nuevo Juego")
+        self.new_game_window.title("New Game")
         # self.new_game_window.attributes("-topmost", True)
         # self.new_game_window.grab_set()
         self.new_game_window.protocol("WM_DELETE_WINDOW", self.new_game_window.withdraw)
@@ -148,25 +147,25 @@ class App(tk.Frame):
     
         player1_var = tk.StringVar(self.new_game_window)
         player2_var = tk.StringVar(self.new_game_window)
-        listbox_player1 = tk.OptionMenu(self.new_game_window, player1_var, "IA")
-        listbox_player2 = tk.OptionMenu(self.new_game_window, player2_var, "IA", "Persona")
+        listbox_player1 = tk.OptionMenu(self.new_game_window, player1_var, "Agent")
+        listbox_player2 = tk.OptionMenu(self.new_game_window, player2_var, "Agent", "Human")
         player1_depth_var = tk.StringVar(self.new_game_window)
         player2_depth_var = tk.StringVar(self.new_game_window)
         listbox_player1_depth = tk.OptionMenu(self.new_game_window, player1_depth_var, "1", "2", "3", "4", "5")
         listbox_player2_depth = tk.OptionMenu(self.new_game_window, player2_depth_var, "1", "2", "3", "4", "5")
         player1_depth_var.set("1")
         player2_depth_var.set("1")
-        player1_var.set("IA")
-        player2_var.set("IA")
+        player1_var.set("Agent")
+        player2_var.set("Agent")
         listbox_player1.config(width=6)
         listbox_player2.config(width=6)
         #listbox_player1_depth.config(state=tk.DISABLED)
         #listbox_player2_depth.config(state=tk.DISABLED)
         player1_var.trace("w",
-            lambda *args: listbox_player1_depth.config(state=tk.NORMAL if player1_var.get() == "IA" else tk.DISABLED)
+            lambda *args: listbox_player1_depth.config(state=tk.NORMAL if player1_var.get() == "Agent" else tk.DISABLED)
         )
         player2_var.trace("w",
-            lambda *args: listbox_player2_depth.config(state=tk.NORMAL if player2_var.get() == "IA" else tk.DISABLED)
+            lambda *args: listbox_player2_depth.config(state=tk.NORMAL if player2_var.get() == "Agent" else tk.DISABLED)
         )
 
         alg_var = tk.StringVar(self.new_game_window)
@@ -206,8 +205,8 @@ class App(tk.Frame):
         self.new_game_window.geometry("{}x{}+{}+{}".format(w, h, int(win_x+(win_w-w)*0.5), int(win_y+(win_h-h)*0.5)))
 
 
-    def draw_checker(self, jugador, x, y, tag=""):
-        c = "#c40003" if jugador == self.PLAYER1 else "white"
+    def draw_checker(self, player, x, y, tag=""):
+        c = "#c40003" if player == self.PLAYER1 else "white"
         self.canvas.create_oval(x, y,
             x+self.cell_size*0.9, y+self.cell_size*0.9,
             fill=c, outline="black", width=1,
@@ -234,7 +233,7 @@ class App(tk.Frame):
         self.canvas.create_text(20, 40,
             text=player1, fill="black", font=(None, 10), anchor="nw"
         )
-        if player1 == "IA":
+        if player1 == "Agent":
             self.canvas.create_text(20, 55,
                 text="Depth: {}".format(search_depth1), fill="black", font=(None, 10), anchor="nw"
             )
@@ -242,7 +241,7 @@ class App(tk.Frame):
         self.canvas.create_text(w-20, 40,
             text=player2, fill="black", font=(None, 10), anchor="ne"
         )
-        if player2 == "IA":
+        if player2 == "Agent":
             self.canvas.create_text(w-20, 55,
                 text="Depth: {}".format(search_depth2), fill="black", font=(None, 10), anchor="ne"
             )
@@ -268,34 +267,34 @@ class App(tk.Frame):
         
         self.board = Board(self.BOARD_HEIGHT, self.BOARD_WIDTH)
 
-        def place(jugador, col, render=True):
-            if self.board.place(jugador, col):
-                print("Player {} places at the {} column".format(1 if jugador == self.PLAYER1 else 2, ordinal(col+1)))
+        def place(player, col, render=True):
+            if self.board.place(player, col):
+                print("Player {} places at the {} column".format(1 if player == self.PLAYER1 else 2, ordinal(col+1)))
                 print(self.board.dump())
                 print("############################################")
                 if render:
                     row = next(r for r in range(self.board.rows) if self.board.occupied(r, col))
-                    self.draw_checker(jugador, 
+                    self.draw_checker(player, 
                         board_pos[0]+(col+0.05)*self.cell_size,
                         board_pos[1]+(row+0.05)*self.cell_size
                     )
                     self.canvas.update()
-                return self.PLAYER2 if jugador == self.PLAYER1 else self.PLAYER1
-            if jugador == "Human":
-                return jugador
-            raise ValueError("IA intenta poner en una columna no valida ({})".format(col))
+                return self.PLAYER2 if player == self.PLAYER1 else self.PLAYER1
+            if player == "Human":
+                return player
+            raise ValueError("Agent tried to place at a invalid column ({})".format(col))
 
-        def human_motion(jugador, x, y, tag="ultimo_movimiento_persona"):
+        def human_motion(player, x, y, tag="last_human_motion"):
             x -= board_pos[0]
             col = int(x/self.cell_size)
             y -= board_pos[1]
             row = int(y/self.cell_size)
-            self.canvas.delete("ultimo_movimiento_persona")
+            self.canvas.delete("last_human_motion")
             if col > -1 and col < self.board.cols and \
                row > -1 and row < self.board.rows and \
                self.board.placeable(col):
                 row = next(r for r in range(self.board.rows-1,-1,-1) if not self.board.occupied(r, col))
-                self.draw_checker(jugador, 
+                self.draw_checker(player, 
                     board_pos[0]+(col+0.05)*self.cell_size,
                     board_pos[1]+(row+0.05)*self.cell_size,
                     tag=tag
@@ -303,15 +302,15 @@ class App(tk.Frame):
                 return col
             return None
             
-        def human_click(jugador, x, y):
-            loc = human_motion(jugador, x, y, "")
+        def human_click(player, x, y):
+            loc = human_motion(player, x, y, "")
             if loc is not None:
-                adversary = place(jugador, loc, render=False)
+                adversary = place(player, loc, render=False)
                 self.canvas.unbind("<Motion>")
                 self.canvas.unbind("<1>")
                 return turn_for(adversary)
 
-        def turn_for(jugador):
+        def turn_for(player):
             if self.terminal_request:
                 return
             if self.board.has_draw():
@@ -323,13 +322,13 @@ class App(tk.Frame):
                 self.prompt("Player {} Wins".format(1 if winner == self.PLAYER1 else 2))
                 print("Player {} wins.".format(1 if winner == self.PLAYER1 else 2))
                 return
-            agent = player1 if jugador == self.PLAYER1 else player2
+            agent = player1 if player == self.PLAYER1 else player2
             if agent == "Human":
-                self.canvas.bind("<Motion>", lambda e: human_motion(jugador, e.x, e.y))
-                self.canvas.bind("<1>", lambda e: human_click(jugador, e.x, e.y))
+                self.canvas.bind("<Motion>", lambda e: human_motion(player, e.x, e.y))
+                self.canvas.bind("<1>", lambda e: human_click(player, e.x, e.y))
                 x = self.canvas.winfo_pointerx()-self.canvas.winfo_rootx()
                 y = self.canvas.winfo_pointery()-self.canvas.winfo_rooty()
-                human_motion(jugador, x, y)
+                human_motion(player, x, y)
             else:
                 print("############################################")
                 if agent == "Random":
@@ -340,12 +339,12 @@ class App(tk.Frame):
                     action = random.choice(m)
                     time.sleep(0.1)
                 else:
-                    action, *_ = search_fn(jugador, self.board, search_depth1 if jugador == self.PLAYER1 else search_depth2)
+                    action, *_ = search_fn(player, self.board, search_depth1 if player == self.PLAYER1 else search_depth2)
                 if action is None:
-                    self.prompt("jugador {} no juega ".format(1 if jugador == self.PLAYER1 else 2))
-                    print("jugador {} no juega".format(1 if jugador == self.PLAYER1 else 2))
+                    self.prompt("Player {} Gives Up".format(1 if player == self.PLAYER1 else 2))
+                    print("Player {} gives up".format(1 if player == self.PLAYER1 else 2))
                 else:
-                    adversary = place(jugador, action)
+                    adversary = place(player, action)
                     # self.master.bind("<Key>", lambda e: turn_for(adversary))
                     return turn_for(adversary)
 
